45e891ad3e90fa0184aa44e6509f6aae5b3df248
weiChao999 <64030588+weiChao999@users.noreply.github.com>
2020-12-07 21:25:16 +0800

    Update Test2.java

--- src/com/huawei/Test2.java
+++ src/com/huawei/Test2.java
@@ -8,5 +8,6 @@ public class Test2 {
         System.out.println("456");
         System.out.println("456");
         System.out.println("456");
+        ssssssssssssssssssssssssssssssssssssss
     }
 }
